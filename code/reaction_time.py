883f6e3699204bf45b7d1a9c9a08080fa7e83fe1
Jamie Boyd <jadobo@gmail.com>
2018-06-08 14:32:38 -0700

    improved reaction task

--- code/reaction_time.py
+++ code/reaction_time.py
@@ -12,56 +12,85 @@ start_pin = 21
 react_pin = 20
 led_pin =26
 
-bounce_time_ms = 10
+bounce_time_ms = 5
+
 max_time = 1000
 inter_trial_time = 3 # +/- 1 second for randomization
 
-name = input ('Subject Name= ')
-name = name.replace(' ', '_')
-num_trials = int (input ('number of trials='))
-data_array = array('f', [0] * num_trials)
+def flash ():
+    for i in range (0, 10):
+        GPIO.output (led_pin, not (GPIO.input (led_pin)))
+        sleep (0.05)
 
-GPIO.setmode (GPIO.BCM)
-GPIO.setup (start_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+def blink ():
+    GPIO.output (led_pin, not (GPIO.input (led_pin)))
+    sleep (1)
+    GPIO.output (led_pin, not (GPIO.input (led_pin)))
 
-GPIO.setup (react_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup (led_pin, GPIO.OUT)
+def main ():
+    name = input ('Subject Name= ')
+    name = name.replace(' ', '_')
+    num_trials = int (input ('number of trials='))
+    data_array = array('f', [0] * num_trials)
 
-GPIO.output (led_pin, GPIO.HIGH)
-for trial in range (0, num_trials):
-    print ('Put finger on start button to start a trial')
-    while True:
-        result = GPIO.wait_for_edge(start_pin, GPIO.RISING, bouncetime= bounce_time_ms, timeout=100)
-        if result is not None:
-            break
+    GPIO.setmode (GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup (start_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+    GPIO.setup (react_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup (led_pin, GPIO.OUT)
+
     GPIO.output (led_pin, GPIO.LOW)
-    print ('Starting trial # ' + str (trial))
-    wait_time = int (1000 * (inter_trial_time + 2 * (0.5 - random())))
-    result = GPIO.wait_for_edge(start_pin, GPIO.FALLING, bouncetime= bounce_time_ms, timeout=wait_time)
-    if result is not None: # subject lifted finger before LED
-        data_array [trial] = -1
-        print ('start position was lifted before LED was lit - trial aborted')
-    else: # subject waited with finger on start switch till start time
-        GPIO.output (led_pin, GPIO.HIGH)
-        start_time = time()
-        result = GPIO.wait_for_edge(react_pin, GPIO.RISING, bouncetime= bounce_time_ms, timeout=max_time)
-        end_time= time()
-        if result is None:
-            data_array [trial] = -2
-            print ('button not pressed before time out')
-        else: # subject pressed the react button before time out
-            data_array [trial] = end_time - start_time
-            print ('Good reaction, time = {:.3f} seconds'.format(data_array [trial]))
+    trial = 0
+    try:
+        for trial in range (0, num_trials):
+            # wait for subject to put finger on start button, sending GPIO high
+            if GPIO.input (start_pin) is GPIO.LOW:
+                print ('Put finger on start button to start a trial')
+                while True:
+                    result = GPIO.wait_for_edge(start_pin, GPIO.RISING, bouncetime= bounce_time_ms, timeout=100)
+                    if result is not None: 
+                        break 
+            GPIO.output (led_pin, GPIO.HIGH) # turn off LED to signal trial start
+            print ('Starting trial # ' + str (trial))
+            wait_time = int (1000 * (inter_trial_time + 2 * (0.5 - random())))
+            result = GPIO.wait_for_edge(start_pin, GPIO.FALLING, bouncetime= bounce_time_ms, timeout=wait_time)
+            if result is not None: # subject lifted finger before LED was lit
+                data_array [trial] = -1
+                print ('Finger was lifted from start button before LED was lit - trial aborted')
+                flash ()
+            else: # subject waited with finger on start switch till start time, so turn on LED and start timing
+                GPIO.output (led_pin, GPIO.LOW)
+                start_time = time()
+                result = GPIO.wait_for_edge(react_pin, GPIO.RISING, bouncetime= bounce_time_ms, timeout=max_time)
+                end_time= time()
+                if result is None:
+                    data_array [trial] = -2
+                    print ('Reaction button not pressed before time out')
+                    flash ()
+                else: # subject pressed the react button before time out
+                    data_array [trial] = end_time - start_time
+                    print ('Measured reaction, time = {:.3f} seconds'.format(data_array [trial]))
+                    GPIO.output (led_pin, GPIO.HIGH) # turn off LED to signal good trial
+                    sleep (0.5)
+                    GPIO.output (led_pin, GPIO.LOW) # turn LED back on again for start of next trial
+    except KeyboardInterrupt:
+        num_trials = trial
+        print ('Remaining trials cancelled after ' + str (num_trials) + ' trials')
+    finally:
+        flash()
+        GPIO.cleanup()
 
-GPIO.output (led_pin, GPIO.LOW)
-GPIO.cleanup()
+
+    dt = datetime.fromtimestamp(time())
+    date_str = '{:04}_'.format(dt.year) + '{:02}_'.format (dt.month) + '{:02}'.format (dt.day)
+    file_name = '/home/pi/Desktop/' + name + '_' + date_str + ".txt"
+    with open (file_name, 'a') as out_file:
+        for trial in range (0, num_trials):
+            out_file.write ('{:.3f}\n'.format (data_array [trial]))
+        out_file.close()
+        print ('Finished. Results printed to ' + file_name)
 
 
-dt = datetime.fromtimestamp(time())
-date_str = '{:04}_'.format(dt.year) + '{:02}_'.format (dt.month) + '{:02}'.format (dt.day)
-file_name = '/home/pi/Desktop/' + name + '_' + date_str + ".txt"
-with open (file_name, 'a') as out_file:
-    for trial in range (0, num_trials):
-        out_file.write ('{:.3f}\n'.format (data_array [trial]))
-    out_file.close()
-    print ('Finished. Results printed to ' + file_name)
+if __name__ == '__main__': # if this is the main file opened, run the main () function
+   main()
