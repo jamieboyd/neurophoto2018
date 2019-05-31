4b70fbbe7dbdf355053ab7e02679d9a0b91b8704
Jamie Boyd <jadobo@gmail.com>
2019-05-31 15:30:09 -0700

    added 501 lecture

--- code/hello_world.py
+++ code/hello_world.py
@@ -2,6 +2,11 @@
 #-*-coding: utf-8 -*-
 """
 The cannonical first program, hello world,
-prints hello world
+prints hello world.
+Note the brackets () around the string to be printed.
+The brackets are used when calling the print FUNCTION
+in Python version 3, but are NOT used with the print
+statement in Python version 2.
+https://docs.python.org/3/whatsnew/3.0.html
 """
 print ('Hello World !')
